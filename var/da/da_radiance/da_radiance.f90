56b4371b2c2ba9333684e4330816edd5e5b98546
WangHaoyang <1452019523@qq.com>
2024-06-01 16:45:58 +0800

    加入了RTTOV_SCATT_ad.inc来调用伴随模式，并对正向、切线性模式进行错误修改

--- var/da/da_radiance/da_radiance.f90
+++ var/da/da_radiance/da_radiance.f90
@@ -23,6 +23,9 @@ module da_radiance
 #ifdef RTTOV
    use module_radiance, only : coefs, rttov_coefs, rttov_profile, rttov_radiance, &
       rttov_transmission,errorstatus_success,gas_id_watervapour,rttov_emissivity
+   #ifdef rttov_scatt
+      use module_radiance, only : coefs_scatt,rttov_scatt_coef,rttov_profile_cloud 
+   #endif
 #endif
 #ifdef CRTM
    use module_radiance, only : crtm_channelinfo_type, crtm_platform_name, crtm_init, &
@@ -89,6 +92,9 @@ module da_radiance
    use da_reporting, only : da_message, da_warning, message, da_error
 #ifdef RTTOV
    use da_rttov, only : da_rttov_init, da_get_innov_vector_rttov 
+   #ifdef RTTOV_SCATT
+      use da_rttov_scatt, only : da_rttov_scatt_init,da_get_innov_vector_rttov
+   #endif
 #endif
    use da_statistics, only : da_stats_calculate
    use da_tools, only : da_residual, da_obs_sfc_correction, &
